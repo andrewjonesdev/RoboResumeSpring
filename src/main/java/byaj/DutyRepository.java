65a1e8f66f84bb2c13bf802fbdc59ef2835732a1
Andrew Jones <jones.gaffney1920@gmail.com>
2017-06-27 16:32:55 -0400

    added repositories

---
+++ src/main/java/byaj/DutyRepository.java
@@ -0,0 +1,13 @@
+package byaj;
+
+/**
+ * Created by student on 6/20/17.
+ */
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface DutyRepository extends CrudRepository<Duty, Integer> {
+
+}
---
+++ src/main/java/byaj/ResumeRepository.java
@@ -0,0 +1,13 @@
+package byaj;
+
+/**
+ * Created by student on 6/20/17.
+ */
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ResumeRepository extends CrudRepository<Resume, Integer> {
+
+}
