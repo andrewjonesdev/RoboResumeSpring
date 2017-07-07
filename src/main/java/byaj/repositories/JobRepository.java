9dc915e5edc168e7ac52d4bd2997cc42ffe6e85b
Andrew Jones <jones.gaffney1920@gmail.com>
2017-07-07 17:57:28 -0400

    updated job start year

--- src/main/java/byaj/repositories/JobRepository.java
+++ src/main/java/byaj/repositories/JobRepository.java
@@ -16,6 +16,6 @@ public interface JobRepository extends CrudRepository<Job, Integer> {
     public List<Job> findAllByJobResOrderByJobStartYearDescJobStartMonthDesc(int num);
     public List <Job> findAllByOrderByJobTitleAscJobEmployerAsc();
     public List <Job> findAllByJobTitleOrderByJobStartYearDescJobStartMonthDesc(String jobTitle);
-    public List <Job> findAllByJobTitleOrderByJobStartYearDesc();
+    public List <Job> findAllByJobTitleOrderByJobStartYearDesc(String jobTitle);
     public List<Job> findAllByJobEmployerOrderByJobResAsc(String employer);
 }
